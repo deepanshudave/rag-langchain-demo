d0f3ee6969e91577f7554f2e71af134df30e3728
Deepanshu <deepanshu.mgm@gmail.com>
2025-08-13 14:48:18 +1000

    third commit

--- setup.sh
+++ setup.sh
@@ -13,19 +13,3 @@ pip install -r requirements.txt
 # Create .env file
 cp .env.example .env
 echo "Update .env file and add your ANTHROPIC_API_KEY"
-
-###################
-# Indexer Command #
-###################
-# python -m indexer.main directory source_documents/
-# python -m indexer.main file source_documents/sample_document.txt
-# python -m indexer.main status
-# python -m indexer.main clear
-# python -m indexer.main force source_documents/
-
-
-###################
-# Search Command  #
-###################
-# python -m search.main ask "What is machine learning?"
-# python -m search.main ask "What is price of CHLORINATOR"
